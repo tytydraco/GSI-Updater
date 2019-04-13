930f81dcc18805dd48779749959f682c049aabb0
Tyler Nijmeh <tylernij@gmail.com>
2019-04-12 22:13:52 -0700

    gsi-updater: Remove test mode and fix writing files
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- gsi-updater.sh
+++ gsi-updater.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 
 # Tune these according to your preferences
-test_mode=true
 gapps="gapps"	# Optional: gapps, gapps-go
 
 # Parameters for the device tree repo
@@ -16,13 +15,6 @@ hw_overlay_user="phhusson"
 hw_overlay_fork="vendor_hardware_overlay"
 hw_overlay_raw="https://raw.githubusercontent.com/$hw_overlay_user/$hw_overlay_fork/$hw_overlay_branch"
 
-# Use local system directory when testing
-if [ "$test_mode" = "true" ]; then
-	system="."
-else
-	system="/"
-fi
-
 get_dt_from_tree() {
 	echo "$(wget -q --no-check-certificate -O- $dt_raw/$1 \
 		| grep -o 'device/phh/treble/.*:system.*' | sed 's/device\/phh\/treble\///' \
@@ -33,6 +25,19 @@ get_hw_overlay_from_tree() {
 	echo "$(wget -q --no-check-certificate -O- $hw_overlay_raw/$1 \
 		| grep -o 'vendor/hardware_overlay/.*:system.*' | sed 's/vendor\/hardware_overlay\///' \
 		| sed 's/\\//')"
+}
+
+write_files() {
+	[ ! -z "$1" ] && while read -r line; do
+		lh="$(echo "$line" | sed 's/:.*//')"
+		rh="$(echo "$line" | sed 's/.*://')"
+		echo $lh \> $system/$rh
+		mkdir -p "$(dirname /$rh)"
+		type=$(echo $lhs | sed 's/\/.*//')
+		[ "$2" = "device" ] && current_raw=$dt_raw
+		[ "$2" = "vendor" ] && current_raw=$hw_overlay_raw
+		wget -q --no-check-certificate -O- $current_raw/$lh > /$rh
+	done <<< "$1"
 }
 
 mount_system() {
@@ -48,21 +53,13 @@ hw_overlay_files="$(get_hw_overlay_from_tree "overlay.mk")"
 
 # Append files
 newline=$'\n'
-files="$dt_files $newline $gapps_files $newline $hw_overlay_files"
 
 # Pull and write files to system
-[ "$test_mode" = "false" ] && mount_system rw
-[ ! -z "$files" ] && while read -r line; do
-	lh="$(echo "$line" | sed 's/:.*//')"
-	rh="$(echo "$line" | sed 's/.*://')"
-	echo $lh \> $system/$rh
-	mkdir -p "$(dirname $system$rh)"
-	type=$(echo $lhs | sed 's/\/.*//')
-	[ "$type" = "device" ] && current_raw=$dt_raw
-	[ "$type" = "vendor" ] && current_raw=$hw_overlay_raw
-	wget -q --no-check-certificate -O- $current_raw/$lh > $system$rh
-done <<< "$files"
-[ "$test_mode" = "false" ] && mount_system ro
+mount_system rw
+write_files "$dt_files" "device"
+write_files "$gapps_files" "device"
+write_files "$hw_overlay_files" "vendor"
+mount_system ro
 
 # Update changes using rw-system
 /system/bin/rw-system.sh
